ca0262c2fb3a3b951e979de87065a67566068a00
Alish Dhama <alishdhama5@gmail.com>
2026-03-13 14:00:31 +0530

    fix controller extension

--- app/http/controllers/customers/homeController.js
+++ app/http/controllers/customers/homeController.js
@@ -3,8 +3,8 @@ const Menu = require('../../../models/menu')
 function homeController() {
   return {
     async index(req, res) {
-      const pizzas = await Menu.find()
-      return res.render('home', { pizzas: pizzas })
+      const cakes = await Menu.find()
+      return res.render('home', { cakes: cakes })
     }
   }
 }
