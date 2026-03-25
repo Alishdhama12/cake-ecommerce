5ddcc020432774138cc5e46fb823d1d2b12700c1
Alish Dhama <alishdhama5@gmail.com>
2026-03-25 11:09:41 +0530

    fix server

--- server.js
+++ server.js
@@ -13,98 +13,122 @@ const flash = require('express-flash');
 const passport = require('passport');
 const Emitter = require('events');
 
-const MongoStore = require('connect-mongo');
+const MongoStore = require('connect-mongo')(session);
+// ======================
+// MongoDB Connection
+// ======================
 
+const url = process.env.MONGO_URL;
+console.log("Mongo URL:", url);
 
-// MongoDB connection
-const url = process.env.MONGO_URL || const url = "mongodb+srv://alish:alish123@cluster0.0w72l9o.mongodb.net/ecommerce";
-mongoose.connect(url,{
-  useNewUrlParser:true,
-  useUnifiedTopology:true
+mongoose.connect(url, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
 })
-.then(()=>{
+.then(() => {
   console.log("MongoDB Connected");
 })
-.catch(err=>{
-  console.log("MongoDB Connection Error:",err);
+.catch(err => {
+  console.log("MongoDB Connection Error:", err);
 });
 
+// ======================
+// Event Emitter
+// ======================
 
-// Event emitter
 const eventEmitter = new Emitter();
 app.set('eventEmitter', eventEmitter);
 
+// ======================
+// Session Config
+// ======================
 
-// Session config
 app.use(session({
     secret: process.env.COOKIE_SECRET || "secret",
-    resave:false,
-    saveUninitialized:false,
-    store: MongoStore.create({
-        mongoUrl: url,
-        collectionName: 'sessions'
-    }),
-    cookie:{ maxAge:1000*60*60*24 }
+    resave: false,
+    saveUninitialized: false,
+store: new MongoStore({
+    mongooseConnection: mongoose.connection,
+    collection: 'sessions'
+}),
+
+    cookie: { maxAge: 1000 * 60 * 60 * 24 }
 }));
 
+// ======================
+// Passport Config
+// ======================
 
-// Passport config
 const passportInit = require('./app/config/passport');
 passportInit(passport);
 
 app.use(passport.initialize());
 app.use(passport.session());
 
+// ======================
+// Flash Messages
+// ======================
 
-// Flash
 app.use(flash());
 
+// ======================
+// Template Engine
+// ======================
 
-// Template engine
 app.use(expressLayouts);
-app.set('views', path.join(__dirname,'resources/views'));
-app.set('view engine','ejs');
+app.set('views', path.join(__dirname, 'resources/views'));
+app.set('view engine', 'ejs');
 
+// ======================
+// Static Files
+// ======================
 
-// Static files
 app.use(express.static('public'));
-app.use(express.urlencoded({extended:false}));
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+// ======================
+// Global Middleware
+// ======================
 
-// Global middleware
-app.use((req,res,next)=>{
+app.use((req, res, next) => {
     res.locals.session = req.session;
     res.locals.user = req.user;
     next();
 });
 
-
+// ======================
 // Routes
+// ======================
+
 require('./routes/web')(app);
 
+// ======================
+// Server Start
+// ======================
 
-// Start server
 const PORT = process.env.PORT || 3000;
 
-const server = app.listen(PORT,'0.0.0.0',()=>{
+const server = app.listen(PORT, '0.0.0.0', () => {
     console.log("Server running on port " + PORT);
 });
 
-
+// ======================
 // Socket.io
+// ======================
+
 const io = require('socket.io')(server);
 
-io.on('connection',(socket)=>{
-    socket.on('join',(orderId)=>{
+io.on('connection', (socket) => {
+    socket.on('join', (orderId) => {
         socket.join(orderId);
     });
 });
 
-eventEmitter.on('orderUpdated',(data)=>{
-    io.to(`order_${data.id}`).emit('orderUpdated',data);
+eventEmitter.on('orderUpdated', (data) => {
+    io.to(`order_${data.id}`).emit('orderUpdated', data);
 });
 
-eventEmitter.on('orderPlaced',(data)=>{
-    io.to('adminRoom').emit('orderPlaced',data);
+eventEmitter.on('orderPlaced', (data) => {
+    io.to('adminRoom').emit('orderPlaced', data);
 });
